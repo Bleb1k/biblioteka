a59d49c110a04f4e785777a1e921b6527a55b96e
Bleb1k <viktor.masnikov17283940@gmail.com>
2024-01-26 19:58:28 +0900

    query of books, untested

--- src/controllers/bookshelf.ts
+++ src/controllers/bookshelf.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Ctx, Get, Post } from 'amala'
+import { Body, Controller, Ctx, Get, Post, Query } from 'amala'
 import { Context } from 'koa'
 import { Book, BookModel, findOrCreateBook } from '@/models/Book'
 import { forbidden } from '@hapi/boom'
@@ -14,6 +14,12 @@ export default class BookshelfController {
   @Get('/')
   async books() {
     const books = await BookModel.find()
+    return books.map((book) => book.strippedAndFilled())
+  }
+
+  @Get('/search')
+  async search(@Query('name') name: string, @Query('author') author?: string) {
+    const books = await BookModel.find({ name, author })
     return books.map((book) => book.strippedAndFilled())
   }
 }
